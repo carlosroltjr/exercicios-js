6c8ecda5bf9d213c6d195a995404514ac5b39faf
Carlos De Rolt Junior <carlos.junior@bry.com.br>
2020-08-31 18:04:34 -0300

    home and product-crud views

--- angular/frontend/src/app/app.module.ts
+++ angular/frontend/src/app/app.module.ts
@@ -1,23 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { AppRoutingModule } from './app-routing.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/template/header/header.component'
+import { FooterComponent } from './components/template/footer/footer.component'
+import { NavComponent } from './components/template/nav/nav.component'
+import { HomeComponent } from './views/home/home.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component'
 
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card';
+import { GreenDirective } from './directives/green.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    GreenDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
